ee615ae9015c73d7827a68426664f35260f2f95c
hsuan-ming Yang <yang5664@gmail.com>
2020-02-18 01:40:28 +0800

    check bugs

--- main.go
+++ main.go
@@ -4,4 +4,6 @@ import Util "github.com/xuan-goldenf/vegastools/util"
 
 func main() {
 	Util.Echo("hello")
+
+	Util.ShowMessage("hi")
 }
--- util/print.go
+++ util/print.go
@@ -3,3 +3,7 @@ package util
 func Echo(str string) {
 	println(str)
 }
+
+func ShowMessage(str string) {
+	println(str)
+}
